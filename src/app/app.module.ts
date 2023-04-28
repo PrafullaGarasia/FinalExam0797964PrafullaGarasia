fd8de33d922ba0a6dcf66bd38a9cc22095dccf5d
Prafulla Kalusinh Garasia <W0797964@myscc.ca>
2023-04-28 18:20:26 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MessageBoardComponent } from './message-board/message-board.component';
+import { MessageCreateComponent } from './message-create/message-create.component';
+import { MessageItemComponent } from './message-item/message-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentCardComponent
+    ContentCardComponent,
+    PageNotFoundComponent,
+    MessageBoardComponent,
+    MessageCreateComponent,
+    MessageItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/message-create/message-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-create',
+  templateUrl: './message-create.component.html',
+  styleUrls: ['./message-create.component.sass']
+})
+export class MessageCreateComponent {
+
+}
